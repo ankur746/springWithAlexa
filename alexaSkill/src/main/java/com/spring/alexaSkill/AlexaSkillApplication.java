c386396292dbd2d7cdca06971da7bbd29a5bedb2
ankur746 <ankurparmar746@gmail.com>
2018-12-11 23:56:26 +0530

    alexaSpring intial project is addded

---
+++ alexaSkill/src/main/java/com/spring/alexaSkill/AlexaSkillApplication.java
@@ -0,0 +1,12 @@
+package com.spring.alexaSkill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexaSkillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexaSkillApplication.class, args);
+	}
+}
